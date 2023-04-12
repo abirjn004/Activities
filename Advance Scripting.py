ac9e216bdd414de23240b3b35dc6be1a8dfc3a69
abirjn004 <124574250+abirjn004@users.noreply.github.com>
2023-04-12 17:31:19 +0530

    main

--- Advance Scripting.py
+++ Advance Scripting.py
@@ -16,3 +16,6 @@ for i in range(A):
     cap=input("CAP Rank: ")
     print()
     print("Hi {},\n\nCongratulations...!\n{}, You got selected for next round of recruitment process submit your academic credential including primary and secondary certificates. Your admission ID is {} and will be eligible for the next round of interview with a CAP rank of {} If you submit all the documents on time and process university might offer you a scholarship.".format(n.capitalize(),n.capitalize(),ID,cap))
+
+    
+  
